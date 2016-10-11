1bf5cf1039988a80469754a62de886c5926f9b44
Paolo Bueno <paolohaji@gmail.com>
2016-10-11 09:28:18 -0300

    Fix datetime bindings and add unit tests

--- dist/appform-field-datetime.tpl.html.js
+++ dist/appform-field-datetime.tpl.html.js
@@ -7,32 +7,31 @@ try {
 
 ngModule.run(['$templateCache', function ($templateCache) {
   $templateCache.put('wfm-template/appform-field-datetime.tpl.html',
-    '\n' +
-    '<p class="md-caption">{{fieldDateCtrl.field.name}}</p>\n' +
     '<div layout="row">\n' +
-    '  <md-input-container flex class="md-block" class="appform-field-number">\n' +
+    '  <p class="md-caption">{{fieldDateTimeCtrl.field.name}}</p>\n' +
+    '  <md-input-container flex class="md-block appform-field-number">\n' +
     '    <label for="inputDate" class="">Date</label>\n' +
     '    <input type="date"\n' +
-    '      placeholder="{{fieldDateCtrl.field.helpText}}"\n' +
+    '      placeholder="{{fieldDateTimeCtrl.field.helpText}}"\n' +
     '      name="inputDate"\n' +
-    '      ng-model="fieldDateCtrl.field.value.date"\n' +
-    '      ng-change="fieldDateCtrl.updateModel()"\n' +
-    '      min="{{fieldDateCtrl.field.fieldOptions.validation.min}}"\n' +
-    '      max="{{fieldDateCtrl.field.fieldOptions.validation.max}}"\n' +
-    '      ng-required="fieldDateCtrl.field.required"\n' +
+    '      ng-model="fieldDateTimeCtrl.field.date"\n' +
+    '      ng-change="fieldDateTimeCtrl.updateModel()"\n' +
+    '      min="{{fieldDateTimeCtrl.field.fieldOptions.validation.min}}"\n' +
+    '      max="{{fieldDateTimeCtrl.field.fieldOptions.validation.max}}"\n' +
+    '      ng-required="fieldDateTimeCtrl.field.required"\n' +
     '    >\n' +
     '  </md-input-container>\n' +
     '\n' +
-    '  <md-input-container flex class="md-block" class="appform-field-number">\n' +
+    '  <md-input-container flex class="md-block appform-field-number">\n' +
     '    <label for="inputTime" class="">Time</label>\n' +
     '    <input type="time"\n' +
-    '           placeholder="{{fieldDateCtrl.field.helpText}}"\n' +
+    '           placeholder="{{fieldDateTimeCtrl.field.helpText}}"\n' +
     '           name="inputTime"\n' +
-    '           ng-model="fieldDateCtrl.field.value.time"\n' +
-    '           ng-change="fieldDateCtrl.updateModel()"\n' +
-    '           min="{{fieldDateCtrl.field.fieldOptions.validation.min}}"\n' +
-    '           max="{{fieldDateCtrl.field.fieldOptions.validation.max}}"\n' +
-    '           ng-required="fieldDateCtrl.field.required"\n' +
+    '           ng-model="fieldDateTimeCtrl.field.time"\n' +
+    '           ng-change="fieldDateTimeCtrl.updateModel()"\n' +
+    '           min="{{fieldDateTimeCtrl.field.fieldOptions.validation.min}}"\n' +
+    '           max="{{fieldDateTimeCtrl.field.fieldOptions.validation.max}}"\n' +
+    '           ng-required="fieldDateTimeCtrl.field.required"\n' +
     '    >\n' +
     '  </md-input-container>\n' +
     '</div>\n' +
